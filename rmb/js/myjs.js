6bd833bf0ac88c3b663864128abf2254e9e6c57c
huybn9493 <35655475+huybn9493@users.noreply.github.com>
2018-04-09 17:21:23 +0700

    9/4

---
+++ rmb/js/myjs.js
@@ -0,0 +1,23 @@
+
+
+ $(document).ready(function(){               
+  $('.materialboxed').materialbox();
+  $('#modal1').modal();
+  $('.collapsible').collapsible();
+});
+
+
+ $(window).load(function() {
+  $('.post-module').hover(function() {
+    $(this).find('.description').stop().animate({
+      height: "toggle",
+      opacity: "toggle"
+    }, 300);
+  });
+});
+
+
+$("#leftside-navigation .sub-menu > a").click(function(e) {
+  $("#leftside-navigation ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
+  e.stopPropagation()
+})
